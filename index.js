90eb7a6bfb9aa617cdeaf54aed22c18a3366ed8c
James Sullivan <jsullivan@cloudanswers.com>
2018-12-27 00:05:43 +0100

    move routes to own file

--- index.js
+++ index.js
@@ -76,101 +76,13 @@ app.get("/auth/callback", async (req, res) => {
   res.redirect("/home");
 });
 
-function getConnection(session) {
-  return new jsforce.Connection({
-    oauth2: { oauth2 },
-    accessToken: session.accessToken,
-    instanceUrl: session.instanceUrl
-  });
-}
-
-async function q(session, soql) {
-  return getConnection(session).query(soql);
-}
-
-app.get("/api/user_count", async (req, res) => {
-  s3.listObjectsV2({ Bucket: "officer-k", Prefix: "salesforce/" })
-    .promise()
-    .then(buckets => {
-      res.json(buckets.KeyCount);
-    })
-    .catch(e => res.status(400).json({ error: e }));
-});
-
-app.get("/api/settings", async (req, res) => {
-  // TODO implement auth redirect in middleware
-  if (!req.session.keyPrefix) {
-    return res.status(401).send({ error: "auth required" });
-  }
-
-  // TODO enforce schema + defaults
-  let content;
-  try {
-    content = await s3
-      .getObject({
-        Bucket: process.env.S3_BUCKET_NAME,
-        Key: path.join(req.session.keyPrefix, "settings")
-      })
-      .promise();
-  } catch (e) {
-    if (`${e}`.indexOf("NoSuchKey") > -1) res.json({});
-    else res.status(500).json({ error: e });
-    return;
-  }
-
-  res.send(content.Body.toString());
-});
-
-app.post("/api/settings", async (req, res) => {
-  if (!req.session.keyPrefix) {
-    return res.redirect("/auth");
-  }
-  // todo enfore schema, size
-  // todo get settings and merge json data so we can toggle individual settings
-
-  console.log(req.body);
-
-  try {
-    await s3
-      .putObject({
-        Bucket: process.env.S3_BUCKET_NAME,
-        Key: path.join(req.session.keyPrefix, "settings"),
-        Body: JSON.stringify(req.body)
-      })
-      .promise();
-  } catch (e) {
-    res.status(400).json({ error: "error saving settings: " + e });
-  }
-
-  let content = await s3
-    .getObject({
-      Bucket: process.env.S3_BUCKET_NAME,
-      Key: path.join(req.session.keyPrefix, "settings")
-    })
-    .promise();
-  res.send(content.Body.toString());
-});
-
-// JUST FOR TESTING
-app.get("/api/session", async (req, res) => {
-  res.json(req.session);
-});
-
-app.get("/api/profile", async (req, res) => {
-  if (!req.session.accessToken) {
-    return res.redirect("/auth");
-  }
-  // FIXME
-  let soql = "SELECT id, name FROM user LIMIT 1";
-  return res.json(await q(req.session, soql));
-});
+app.use("/api", require("./routes/api"));
 
 // app.use(express.static(path.join(__dirname, "frontend/build")));
 app.get("*", (req, res) => {
-  if (!process.env.LOCALDEV)
-    return res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
-  var newurl = "http://localhost:5100" + req.url;
-  request(newurl).pipe(res);
+  return res.sendFile(path.join(__dirname + "/frontend/build/index.html"));
+  // var newurl = "http://localhost:5100" + req.url;
+  // request(newurl).pipe(res);
 });
 
 app.listen(PORT, function() {
---
+++ routes/api.js
@@ -0,0 +1,96 @@
+const express = require("express");
+const AWS = require("aws-sdk");
+const s3 = new AWS.S3();
+
+function getConnection(session) {
+  return new jsforce.Connection({
+    oauth2: { oauth2 },
+    accessToken: session.accessToken,
+    instanceUrl: session.instanceUrl
+  });
+}
+
+async function q(session, soql) {
+  return getConnection(session).query(soql);
+}
+
+const app = express.Router();
+
+app.get("/user_count", async (req, res) => {
+  s3.listObjectsV2({ Bucket: "officer-k", Prefix: "salesforce/" })
+    .promise()
+    .then(buckets => {
+      res.json(buckets.KeyCount);
+    })
+    .catch(e => res.status(400).json({ error: e }));
+});
+
+app.get("/settings", async (req, res) => {
+  // TODO implement auth redirect in middleware
+  if (!req.session.keyPrefix) {
+    return res.status(401).send({ error: "auth required" });
+  }
+
+  // TODO enforce schema + defaults
+  let content;
+  try {
+    content = await s3
+      .getObject({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: path.join(req.session.keyPrefix, "settings")
+      })
+      .promise();
+  } catch (e) {
+    if (`${e}`.indexOf("NoSuchKey") > -1) res.json({});
+    else res.status(500).json({ error: e });
+    return;
+  }
+
+  res.send(content.Body.toString());
+});
+
+app.post("/settings", async (req, res) => {
+  if (!req.session.keyPrefix) {
+    return res.redirect("/auth");
+  }
+  // todo enfore schema, size
+  // todo get settings and merge json data so we can toggle individual settings
+
+  console.log(req.body);
+
+  try {
+    await s3
+      .putObject({
+        Bucket: process.env.S3_BUCKET_NAME,
+        Key: path.join(req.session.keyPrefix, "settings"),
+        Body: JSON.stringify(req.body)
+      })
+      .promise();
+  } catch (e) {
+    res.status(400).json({ error: "error saving settings: " + e });
+  }
+
+  let content = await s3
+    .getObject({
+      Bucket: process.env.S3_BUCKET_NAME,
+      Key: path.join(req.session.keyPrefix, "settings")
+    })
+    .promise();
+  res.send(content.Body.toString());
+});
+
+// JUST FOR TESTING
+app.get("/session", async (req, res) => {
+  res.json(req.session);
+});
+
+app.get("/api/profile", async (req, res) => {
+  if (!req.session.accessToken) {
+    return res.redirect("/auth");
+  }
+  // FIXME
+  let soql = "SELECT id, name FROM user LIMIT 1";
+  return res.json(await q(req.session, soql));
+});
+
+module.exports = app;
