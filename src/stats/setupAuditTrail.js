a7a2d7b640adc8705bfa4af35f5e27332fa7f89d
James Sullivan <jsullivan@cloudanswers.com>
2018-12-30 11:20:34 +0100

    WIP audit trail stats

--- src/stats/setupAuditTrail.js
+++ src/stats/setupAuditTrail.js
@@ -1,17 +1,29 @@
+var groupBy = function(xs, key) {
+  return xs.reduce(function(rv, x) {
+    (rv[x[key]] = rv[x[key]] || []).push(x);
+    return rv;
+  }, {});
+};
+
 async function setupAuditTrail(keyPrefix, conn, storage) {
   let history = await conn
     .sobject("SetupAuditTrail")
-    .select("*")
+    .select("*, CreatedBy.Username")
     .execute();
-  console.log(history);
-  // TODO save audit history so user can get it after 6 months of history
-  process.exit();
+
+  // backup audit history since salesforce deletes it after 6 months
+  storage.archive(keyPrefix, "SetupAuditTrail", history);
+
+  let actions = Object.keys(groupBy(history, "Action"));
+  let impersonatedUsers = Object.keys(groupBy(history, "DelegateUser"));
+
   return {
-    utilization: 0.99,
     html: `
       <div>
         <h3>System Audit Trail Changes</h3>
-        ${JSON.stringify(history)}
+        <div>${JSON.stringify(actions.join("<br/>"))}</div>
+        <h3>Impersonated Users</h3>
+        <div>${JSON.stringify(impersonatedUsers).join("<br/>")}</div>
       </div>
     `
   };
